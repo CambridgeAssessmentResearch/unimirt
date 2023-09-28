4e934fb260bba3d0293956116eb9ea58e950d1fb
TomBentonCA <32639384+TomBentonCA@users.noreply.github.com>
2023-09-28 14:32:41 +0100

    Fixed rounding issues in classical app

--- inst/shiny-examples/ClassicalAppV4.R
+++ inst/shiny-examples/ClassicalAppV4.R
@@ -187,14 +187,14 @@ server <- function(input, output,session) {
       Value=c(ncol(filedata$scoreddata),sum(ild()$Max.mark)
               ,floor(100*mean(tots))/100
               ,floor(100*sd(tots))/100
-              ,floor(1000*alpha+0.5)/1000
-              ,floor(1000*guttman()$guttman+0.5)/1000
-              ,floor(1000*sd1*sqrt(1-alpha)+0.5)/1000
-              ,floor(1000*sd1*sqrt(alpha*(1-alpha))+0.5)/1000
-              ,floor(1000*sd1*sqrt(1-alpha^2)+0.5)/1000
-              ,floor(1000*sd1*sqrt(1-guttman()$guttman)+0.5)/1000
-              ,floor(1000*sd1*sqrt(guttman()$guttman*(1-guttman()$guttman))+0.5)/1000
-              ,floor(1000*sd1*sqrt(1-guttman()$guttman^2)+0.5)/1000
+              ,floor(100*alpha+0.5)/100
+              ,floor(100*guttman()$guttman+0.5)/100
+              ,floor(100*sd1*sqrt(1-alpha)+0.5)/100
+              ,floor(100*sd1*sqrt(alpha*(1-alpha))+0.5)/100
+              ,floor(100*sd1*sqrt(1-alpha^2)+0.5)/100
+              ,floor(100*sd1*sqrt(1-guttman()$guttman)+0.5)/100
+              ,floor(100*sd1*sqrt(guttman()$guttman*(1-guttman()$guttman))+0.5)/100
+              ,floor(100*sd1*sqrt(1-guttman()$guttman^2)+0.5)/100
       )
     )
   })  
@@ -222,9 +222,9 @@ server <- function(input, output,session) {
     max1=sum(ild()$Max.mark)
     distdata1=data.frame(Score=0:max1,
                         N=tabulate(tots+1,max1+1))
-    distdata1$Per_cent=floor(1000*distdata1$N/sum(distdata1$N)+0.5)/100
+    distdata1$Per_cent=floor(1000*distdata1$N/sum(distdata1$N)+0.5)/10
     distdata1$Cum_per_cent=floor(1000*sapply(distdata1$Score
-                                 ,function(i){0+mean(tots>=i)})+0.5)/100
+                                 ,function(i){0+mean(tots>=i)})+0.5)/10
     distdata1
 
       })
